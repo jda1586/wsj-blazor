056bf58312f2a8fde08e9ccf9350f81eaf873af6
Pedro Luna <neko.hitokori@gmail.com>
2023-10-18 01:09:11 -0600

    Init Project

---
+++ wsj.Domain/Entities/Invitation.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace wsj.Domain.Entities;
+
+public class Invitation
+{
+    [Required] public int Id { get; set; }
+    [Required] public string Name { get; set; }
+    [Required] public bool Accept { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ wsj.Persistence/Contexts/WsjDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using wsj.Domain.Entities;
+
+namespace wsj.Persistence.Contexts;
+
+public class WsjDbContext : DbContext
+{
+    public WsjDbContext(DbContextOptions<WsjDbContext> options) : base(options)
+    {
+        
+    }
+
+    public virtual DbSet<Invitation> Invitations { get; set; }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
+    {
+        ChangeTracker.DetectChanges();
+        var date = DateTime.UtcNow;
+
+        foreach (var entry in ChangeTracker.Entries().Where(e => e.State == EntityState.Modified))
+        {
+            entry.Property("UpdatedAt").CurrentValue = date;
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
